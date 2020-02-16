9f7ada9393da62c5fc4726c4f0560652eef2fbbd
martinmoeller <martinmoeller>
2020-02-16 09:07:50 +0100

    small improvements

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ static GRADLEW: &str = "gradlew.bat";
 
 fn main() {
     #[cfg(windows)]
-        ctrlc::set_handler(move || {
+    ctrlc::set_handler(move || {
         // ignore SIGINT and let the child process handle it
         // this is required for windows batch "Terminate batch job (Y/N)"
     })
-        .expect("Error setting Ctrl-C handler");
+    .expect("Error setting Ctrl-C handler");
 
     let current_dir = env::current_dir().expect("no current dir :-9?");
 
@@ -26,45 +26,35 @@ fn main() {
             eprint!("Did not find gradlew wrapper!");
             exit(1)
         }
-        Some((wrapper, dir)) => execute(&wrapper, dir)
+        Some((wrapper, dir)) => execute(&wrapper, dir),
     }
 }
 
 fn find_wrapper(dir: PathBuf) -> Option<(PathBuf, PathBuf)> {
-    let found = find_wrapper_recursive(&dir);
+    let found = find_wrapper_in_dir(&dir);
 
     match found {
         Some(wrapper) => Some((wrapper, dir)),
         None => match dir.parent() {
             Some(parent) => find_wrapper(parent.to_path_buf()),
-            None => None
+            None => None,
         },
     }
 }
 
-fn find_wrapper_recursive(dir: &PathBuf) -> Option<PathBuf> {
+fn find_wrapper_in_dir(dir: &PathBuf) -> Option<PathBuf> {
     let files = fs::read_dir(dir).expect("Failed to list contents of ");
-    for file in files {
-        match file {
-            Ok(file) => {
-                if file.path().ends_with(PathBuf::from(GRADLEW)) {
-                    return Some(file.path());
-                }
-            }
-            Err(_e) => println!("Error reading dir entry {}", _e),
-        }
-    }
-    None
+
+    files
+        .filter_map(Result::ok)
+        .map(|entry| entry.path())
+        .find(|file| file.ends_with(PathBuf::from(GRADLEW)))
 }
 
 // https://stackoverflow.com/a/53479765
 pub fn execute(gradle_path: &PathBuf, working_directory: PathBuf) {
     let args: Vec<String> = env::args().skip(1).collect();
-    println!(
-        "Executing {} {}",
-        gradle_path.display(),
-        join(&args, " ")
-    );
+    println!("Executing {} {}", gradle_path.display(), join(&args, " "));
 
     let spawn_result = Command::new(gradle_path)
         .current_dir(working_directory)
